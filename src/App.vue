c400bea98f9e08bb9ba254c27140de9d3a90bfc4
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2018-07-06 01:03:03 +0200

    add base styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    test
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  @import styles/app
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-        ? '/vue-spotify/'
-        : '/',
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/vue-spotify/'
+    : '/',
 
-    devServer: {
-        port: 8080
-    },
+  devServer: {
+    port: 8080
+  },
+
+  css: {
+    loaderOptions: {
+      sass: {
+        data: '@import ./src/styles/util/util.sass',
+      }
+    }
+  }
 };
