dbdd9753244438d0e7aad0e3f009af4a18222af0
Grzegorz Kaminski <kaminski_grzegorz@wp.pl>
2019-11-14 23:35:36 +0100

    feat: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  proseWrap: "always",
+  singleQuote: false,
+  trailingComa: "all",
+  tabWdith: 2,
+  useTab: false,
+  endOfLine: "auto"
+};
